eb059d3c8a7d420a0e1af73c81ee8148b7bee008
terejoe <nzeamalunkechinyere@gmail.com>
2023-11-01 13:19:07 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import NewExpenses from './components/NewExpenses/NewExpenses';
+import Expenses from './components/Expenses/Expenses'
+
+const dummy_expenses =[
+  {
+    id: '1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2022, 7, 14)
+  },
+  {
+    id: '2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2020, 6, 15)
+  },
+  {
+    id: '3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2020, 5, 16)
+  },
+  {
+    id: '4',
+    title: 'New Desk(Wooden)',
+    amount: 450,
+    date: new Date(2021, 4, 17)
+  }
+]
+
+function App() {
+  const [expenses, setExpenses] = useState(dummy_expenses);
+
+  const addExpenseHandler= (expense) => {
+    setExpenses((prevExpenses) =>{
+      return [expense, ...prevExpenses]
+    })
+  }
+
+  return (
+    <>
+      <NewExpenses onAddExpense = {addExpenseHandler}/>
+      <Expenses items={expenses}/>
+    </>
+  )
+}
+export default App;
---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import ExpensesList from './ExpensesList';
+import Card  from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+export default function Expenses(props) { 
+  const [filteredYear, setFilteredYear] = useState('2020')
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
+  const filteredExpenses = props.items.filter(expenses => {
+    return expenses.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <li>
+      <Card className = 'expenses'>
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      <ExpensesChart expenses={filteredExpenses}/> 
+      <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </li>
+    
+  )
+}
